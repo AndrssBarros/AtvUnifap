75f0127d5f96e40d6715de8dc2e7174ef822d9b8
AndrssPessoa <103079879+AndrssBarros@users.noreply.github.com>
2023-04-28 00:10:47 -0300

    array.js

---
+++ array.js
@@ -0,0 +1,24 @@
+- Faça um programa que tenha um ARRAY, contendo 3 elementos dentro, e
+utilizando o método SLICE, pegue a primeira letra de cada elemento.
+
+- Faça um array que contenha 5 números, e com a estrutura de repetição
+FOR, e utilizando o IF/ELSE diga qual é o maior número do array.
+- Faça um programa com o loop FOR, que some todos os valores do loop, e
+ao final mostre a média desses valores, e a soma
+
+- Faça um programa com o loop FOR, que some todos os valores do loop, e
+ao final mostre a média desses valores, e a soma
+
+- Faça um array que contenha 5 números, e com a estrutura de repetição
+FOR, e utilizando o IF/ELSE diga qual é o MENOR número do array.
+
+Faça um loop FOR que encontre todos os números PARES, e todos os
+números ÍMPARES, e armazene respectivamente os dois em um array
+denominado PAR e outro ÍMPAR
+dica ( Para sabermos se um número é PAR o seu resto da divisão por 2,
+deve ser igual a 0,
+Para sabermos se um número é ÍMPAR, seu resto da divisão por 2 deve
+ser diferente de 0. exemplo
+1 % 2 == 0 - Impar
+2 % 2 == 0 - Par
+)
